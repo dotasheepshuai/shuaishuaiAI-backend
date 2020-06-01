a1001204322140706fe839430fa923e5bd102a23
Tianyang <dotasheepliu@gmail.com>
2020-05-31 23:37:51 -0700

    query from dynamodb

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB();
+const {isEmpty, random} = require('lodash');
+
 exports.handler = async (event) => {
+    log(event);
     const input = event.queryStringParameters.input;
     const output = await getAIResponse(input);
     return getResponsePayload(output);
 };
 
 async function getAIResponse(input) {
-    return input.replace(/\?/g, '!');
+    const params = {
+        ExpressionAttributeValues: {
+            ":v1": {
+                S: input
+            }
+        },
+        KeyConditionExpression: "Question = :v1",
+        ProjectionExpression: "Answers",
+        TableName: 'Conversation'
+    };
+    const dynamodbResponses = await new Promise((resolve, reject) => {
+        dynamodb.query(params, (error, data) => {
+            if (error) {
+                log(error);
+                reject(error);
+            }
+            const responses = ((data.Items[0] || {}).Answers || {}).SS || [];
+            resolve(responses);
+        });
+    });
+    log(`Input is "${input}", dynamodb responses are "${JSON.stringify(dynamodbResponses)}"`);
+
+    // Stupid path
+    if (isEmpty(dynamodbResponses)) {
+        const stupidOutput = input.replace(/\?/g, '!');
+        log(`Use stupid output "${stupidOutput}" because dynamodb responses are empty.`);
+        return stupidOutput;
+    }
+
+    // Clever path
+    const randomNumber = random(dynamodbResponses.length-1);
+    const cleverOutput = dynamodbResponses[randomNumber];
+    log(`Pick the ${randomNumber}-th value from the list - "${cleverOutput}", as the clever output`);
+    return cleverOutput;
 }
 
 function getResponsePayload(text) {
@@ -16,4 +54,8 @@ function getResponsePayload(text) {
         },
         body: JSON.stringify(text),
     };
+}
+
+function log(text) {
+    console.log(text);
 }
